0b30495cf89ca6d1e10006f7d67c46ce7e4c4d9e
169398 <kulubiidris@gmail.com>
2024-03-27 14:47:09 +0300

    file added

--- components/PostFormCard.js
+++ components/PostFormCard.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Card from "./Card";
 import Avatar from "./Avatar";
 import {useContext, useEffect, useState} from "react";
@@ -70,8 +71,8 @@ export default function PostFormCard({onPost}) {
       {uploads.length > 0 && (
         <div className="flex gap-2">
           {uploads.map(upload => (
-            <div className="mt-2" >
-              <img src={upload} alt="" className="w-auto h-24 rounded-md" />
+            <div key={index} className="mt-2" >
+              <Image src={upload} alt="" className="w-auto h-24 rounded-md" />
             </div>
           ))}
         </div>
